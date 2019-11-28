0e0948c2c3272664ef0059975ba3d17461d3d3d1
thossakrai <thossakrai@outlook.com>
2019-11-28 18:19:22 +0700

    initial commmit

---
+++ CVSdisease.R
@@ -0,0 +1,9 @@
+# Title     : Cardiovascular Disease Prediction
+# Objective : Predicting CVS Disease based on Kaggle's Dataset
+# Dataset : https://www.kaggle.com/sulianova/cardiovascular-disease-dataset
+# Created by: Thossakrai Nakkasem
+# Created on: 28/11/19
+
+cardio <- read.csv("cardio_train.csv")
+str(cardio)
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Project Description
+This Project aims to predict the cardiovascular disease based on dataset containing 70000 obsevations of 12 features.
+
+##List of Features
+
+----
+
+# Developer
+
+Thossakrai Nakkasem (60090065)
+Machine Learning Course
+Software Engineering Program, KMITL.
+2019
+
+---
