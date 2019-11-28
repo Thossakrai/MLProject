059563b80e7c29ddc834dd0cb943ca19cc84e9ba
thossakrai <thossakrai@outlook.com>
2019-11-28 22:55:28 +0700

    train model

--- CVSdisease.R
+++ CVSdisease.R
@@ -1,9 +1,20 @@
 # Title     : Cardiovascular Disease Prediction
-# Objective : Predicting CVS Disease based on Kaggle's Dataset
+# Objective : Predicting CV Disease based on Kaggle's Dataset
 # Dataset : https://www.kaggle.com/sulianova/cardiovascular-disease-dataset
 # Created by: Thossakrai Nakkasem
 # Created on: 28/11/19
 
-cardio <- read.csv("cardio_train.csv")
+
+# read csv file 
+cardio <- read.csv("cardio_train.csv",sep =  ";")
 str(cardio)
 
+#data cleaning
+cholesterol <- factor(cardio$cholesterol,levels = c("1", "2", "3"), ordered = TRUE)
+
+#data exploratory
+summary(cardio$height)
+summary(cardio$weight)
+
+#Train Test Split
+cardio_train <- cardio[[1:49000, ]]
